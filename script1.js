c74f5cb4013fc90a40d83e33a42b2e1877955d51
Jaskirat Singh <112509465+jaskirat-13@users.noreply.github.com>
2022-12-06 00:04:30 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,17 @@
+
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+window.onscroll = () =>{
+  
+    loginForm.classList.remove('active');
+}
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
